c127f4df1de305974dbf596e5fc4757bee74371f
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-11 16:19:20 +0200

    Add printBoard method to print the whole board

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -13,4 +13,16 @@ public class Game {
       }
     }
   }
+
+  public void printBoard() {
+    System.out.println("-------------");
+    for (int i = 0; i < 3; i++) {
+      System.out.print("|");
+      for (int j = 0; j < 3; j++) {
+        System.out.print(board[i][j] + "|");
+      }
+      System.out.println();
+      System.out.println("-------------");
+    }
+  }
 }
